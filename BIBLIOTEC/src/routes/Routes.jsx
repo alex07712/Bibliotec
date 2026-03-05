88e01926cd3a8800d01e67bad78dda3e644140fc
alex07712 <103295553+alex07712@users.noreply.github.com>
2026-03-05 06:27:59 +0000

    arreglar imports App.jsx

--- BIBLIOTEC/src/routes/Routes.jsx
+++ BIBLIOTEC/src/routes/Routes.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home, Gestion, Prestamos } from '../pages'
+import { Layout } from '../layouts'
 
 
 export function Rutas() {
   return (
     <Routes>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/gestion' element={<Gestion/>}/>
-      <Route path='/prestamos' element={<Prestamos/>}/>
+      <Route element={<Layout />}> 
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/gestion' element={<Gestion/>}/>
+        <Route path='/prestamos' element={<Prestamos/>}/>
+        </Route>
     </Routes>
   )
 }
