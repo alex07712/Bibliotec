408abbabaa2cfd88cbcec9aec217df388bebda32
alex07712 <103295553+alex07712@users.noreply.github.com>
2026-03-05 04:43:15 +0000

    confirmo

---
+++ BIBLIOTEC/src/pages/prestamos/Prestamos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Prestamos() {
+  return (
+    <div>Prestamos aqui</div>
+  )
+}
--- BIBLIOTEC/src/routes/Routes.jsx
+++ BIBLIOTEC/src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Home, Gestion } from '../pages'
+import { Home, Gestion, Prestamos } from '../pages'
 
 
 export function Rutas() {
@@ -8,6 +8,7 @@ export function Rutas() {
     <Routes>
       <Route path='/home' element={<Home/>}/>
       <Route path='/gestion' element={<Gestion/>}/>
+      <Route path='/prestamos' element={<Prestamos/>}/>
     </Routes>
   )
 }
