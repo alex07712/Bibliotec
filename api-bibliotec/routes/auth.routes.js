4c434b93ea44d6fd74e216b32cf5907c27661613
Angel Alexander Cano <cano2006castillo@gmail.com>
2026-04-14 23:26:44 -0600

    funciona?

--- api-bibliotec/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/auth.controller");
-const api = express.Router();
-
-api.post("/login", authController.login);
-
-module.exports = api;
